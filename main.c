ca8a6a91c73a696ce0ec85b0feb1f37e44832217
airyou-code <holod7272@outlook.com>
2021-12-06 14:48:44 +0100

    for max

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
     // scannerInit(stdin);
     // set_code_output(stdout);
-
+//
     argc = 3;
     argv[1] = "test_pr2.in";
     argv[2] = "out.out";
@@ -22,9 +22,7 @@ int main(int argc, char *argv[])
     set_code_output(fopen(argv[2], "w"));
 
     int a = start();
-    if (!a)
-        return a;
-
-    return 0;
+    printf("EROR CODE:%d\n", a);
+    return a;
 }
 //./prg.code < test_pr2.in > out.out
