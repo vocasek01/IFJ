acaef512f319fe164f5795b8e1071534a2633df8
Golikov Ivan <60191541+TheSaintDeer@users.noreply.github.com>
2021-12-08 20:22:57 +0100

    Update codegen.h

--- codegen.h
+++ codegen.h
@@ -1,3 +1,10 @@
+/*------------------------------------------
+ Title:  codegen.h
+ Author: Kirillov Artem
+ Login:  xkiril01
+ Date:   23 Nov 2021;
+------------------------------------------*/
+
 #include <stdio.h>
 #include "symtable.h"
 
@@ -5,7 +12,6 @@ int set_code_output(FILE *file);
 int generate_header();
 int generate_func_top(char *function_identifier);
 int generate_func_param(char *param_identifier, int param_pos);
-// int generate_return_values(Symtable_item *function);
 int generate_func_bottom(char *function_identifier);
 int generate_move(char *dest_frame, char *identifier, char *source_frame, char *source);
 int generate_return_move(char *identifier, int position);
